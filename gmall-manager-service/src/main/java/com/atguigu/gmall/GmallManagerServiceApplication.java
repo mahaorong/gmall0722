27d8e7386609488901a0fc7fd9e497ea7f23362a
mahaorong <931380187@qq.com>
2019-12-05 14:14:20 +0800

    12.5 动态数据源

--- gmall-manager-service/src/main/java/com/atguigu/gmall/GmallManagerServiceApplication.java
+++ gmall-manager-service/src/main/java/com/atguigu/gmall/GmallManagerServiceApplication.java
@@ -2,14 +2,19 @@ package com.atguigu.gmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan("com.atguigu.gmall.manager.mapper")
+@ComponentScan(basePackages = "com.atguigu.gmall")
 public class GmallManagerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GmallManagerServiceApplication.class, args);
     }
+
+
 
 }
---
+++ gmall-manager-service/src/main/java/com/atguigu/gmall/manager/config/MybatisConfig.java
@@ -0,0 +1,70 @@
+package com.atguigu.gmall.manager.config;
+
+import com.atguigu.gmall.manager.dds.DynamicDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import tk.mybatis.spring.annotation.MapperScan;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Mybatis配置
+ * @author Louis
+ * @date Oct 31, 2018
+ */
+@Configuration
+@MapperScan(basePackages = {"com.atguigu.gmall.manager.mapper"}) // 扫描mapper
+public class MybatisConfig {
+
+    @Bean("master")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.master")
+    public DataSource master() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean("slave")
+    @ConfigurationProperties(prefix = "spring.datasource.slave")
+    public DataSource slave() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean("dynamicDataSource")
+    public DataSource dynamicDataSource() {
+        DynamicDataSource dynamicDataSource = new DynamicDataSource();
+        Map<Object, Object> dataSourceMap = new HashMap<>(2);
+        dataSourceMap.put("master", master());
+        dataSourceMap.put("slave", slave());
+        // 将 master 数据源作为默认指定的数据源
+        dynamicDataSource.setDefaultDataSource(master());
+        // 将 master 和 slave 数据源作为指定的数据源
+        dynamicDataSource.setDataSources(dataSourceMap);
+        return dynamicDataSource;
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean() throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        // 配置数据源，此处配置为关键配置，如果没有将 dynamicDataSource作为数据源则不能实现切换
+        sessionFactory.setDataSource(dynamicDataSource());
+        sessionFactory.setTypeAliasesPackage("com.atguigu.gmall.bean");    // 扫描Model
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sessionFactory.setMapperLocations(resolver.getResources("classpath*:mapper/*.xml"));    // 扫描映射文件
+        return sessionFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        // 配置事务管理, 使用事务时在方法头部添加@Transactional注解即可
+        return new DataSourceTransactionManager(dynamicDataSource());
+    }
+}
